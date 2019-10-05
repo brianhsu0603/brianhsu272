e4f3558b0bf3da505fc3b4af3a031e34e5bdf43d
brianhsu0603 <56208363+brianhsu0603@users.noreply.github.com>
2019-10-05 16:59:24 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<div class="topnav">
+  <a class="active" href="#home">Home</a>
+  <a href="#news">News</a>
+  <a href="#contact">Contact</a>
+  <a href="#about">About</a>
+</div>
+<?php
+
+echo "hello";
+
+?>
+</body>
+</html>
+
+
+
+
