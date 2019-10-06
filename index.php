9ef9ea049e2c2289e67db9c8c0197c2b3a2f7cc0
brianhsu0603 <56208363+brianhsu0603@users.noreply.github.com>
2019-10-05 21:47:07 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,26 +1,52 @@
+
+
 <!DOCTYPE html>
 <html>
 <head>
+<style>
+
+h1 {color: blue;}
+
+topnav {
+  background-color: #333;
+  overflow: hidden;
+}
+
+topnav a {
+  float: left;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+topnav a.active {
+  background-color: #4CAF50;
+  color: white;
+}
+
+</style>
 </head>
 <body>
+
+<h1>SmartHome</h1>
+
 <div class="topnav">
   <a class="active" href="#home">Home</a>
   <a href="#news">News</a>
   <a href="#contact">Contact</a>
   <a href="#about">About</a>
 </div>
+
 <?php
-
-<<<<<<< HEAD
-
-=======
-echo "hello";
->>>>>>> e4f3558b0bf3da505fc3b4af3a031e34e5bdf43d
-
 ?>
 </body>
 </html>
-
-
 
 
