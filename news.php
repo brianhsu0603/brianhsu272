ae4e90d80e238af68e99aa9925f494f8403122b1
brianhsu0603 <56208363+brianhsu0603@users.noreply.github.com>
2019-10-06 00:54:35 -0700

    Update news.php

--- news.php
+++ news.php
@@ -6,6 +6,7 @@
 <h1>News</h1>
 <pre>
 New Product Coming! 
+
 1.Let fighting robot protect your family or your company properties!
 
 2.House Keeper robot software upadate version 2.0 available now!
