155cb360e8abe9be575f56d259dfefeb032a671c
brianhsu0603 <56208363+brianhsu0603@users.noreply.github.com>
2019-10-05 23:33:17 -0700

    Update contacts.php

--- contacts.php
+++ contacts.php
@@ -3,11 +3,11 @@
 <head>
 </head>
 <body>
-
+<h1>Contacts</h1>
 <?php
-
-echo "helo";
-
+$myfile = fopen("272contacts.txt", "r") or die("Unable to open file!");
+echo fread($myfile,filesize("272contacts.txt"));
+fclose($myfile);
 ?>
 </body>
 </html>
