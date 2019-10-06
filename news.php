bf1a32e97aa7918cd480e3a1eed5194c8fd49f72
brianhsu0603 <56208363+brianhsu0603@users.noreply.github.com>
2019-10-06 01:01:48 -0700

    Update news.php

--- news.php
+++ news.php
@@ -3,6 +3,13 @@
 <head>
 </head>
 <body>
+  <div class="topnav">
+  <a class="active" href="index.php">Home</a>
+  <a href="news.php">News</a>
+  <a href="contacts.php">Contacts</a>
+  <a href="products.php">Products</a>
+  <a href="about.php">About</a>
+</div>
 <h1>News</h1>
 <pre>
 New Product Coming! 
