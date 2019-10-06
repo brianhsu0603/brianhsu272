f2faacec41d6632cd08118f85e85737e4630daa2
brianhsu0603 <56208363+brianhsu0603@users.noreply.github.com>
2019-10-06 00:47:10 -0700

    Update products.php

--- products.php
+++ products.php
@@ -5,16 +5,16 @@
 <body>
 <h1>Products</h1>
 <pre>
-House Keeper Robot:
+1.House Keeper Robot:
 this robot can do all the chores for you, 
 including taking out the garbage, walk your dog and wash your dishes etc.
 
-Security Robot:
+2.Security Robot:
 this robot can detect unfamiliar faces. 
 if it detects one in your house or company when on guard, 
 you will receive an alert.
 
-Voice Assistant:
+3.Voice Assistant:
 this robot combine AI and iot. it will listen to your instructions and 
 do corresponding tasks, for example, turn off the air conditioner, 
 close the window curtain, adjust the brightness of light etc.
