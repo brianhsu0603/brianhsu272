24acb0bbe0e0346df289d75d94807195c757470e
brianhsu0603 <56208363+brianhsu0603@users.noreply.github.com>
2019-10-06 00:36:39 -0700

    Add files via upload

---
+++ about.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<h1>About us</h1>
+<pre>
+We are a startup which is founded in 2018. By using AI and IOT technology, 
+we aim to help our customers to live a smarter, more conveninet and more effiecient life. 
+We raised 1 billion in the financing and has now provided our products and services to 1000+ customers in just a year.
+</pre>
+</body>
+</html>
---
+++ news.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<h1>News</h1>
+<pre>
+New Product Coming! 
+Let fighting robot protect your family or your company properties!
+
+House Keeper robot software upadate version 2.0 available now!
+</pre>
+</body>
+</html>
