691a2bb012b9d4593dec1235f87c6143cfc8b4e4
brianhsu0603 <56208363+brianhsu0603@users.noreply.github.com>
2019-10-06 01:00:45 -0700

    Update contacts.php

--- contacts.php
+++ contacts.php
@@ -3,18 +3,23 @@
 <head>
 </head>
 <body>
+<div class="topnav">
+  <a class="active" href="index.php">Home</a>
+  <a href="news.php">News</a>
+  <a href="contacts.php">Contacts</a>
+  <a href="products.php">Products</a>
+  <a href="about.php">About</a>
+</div>
 <h1>Contacts</h1>
 <?php
 
-  $fn = fopen("272contacts.txt","r");
-  
-  while(! feof($fn))  {
-	$result = fgets($fn);
-	echo $result <br>;
-  }
-
-  fclose($fn);
-
+<?php 
+      $contacts = file('./contacts.txt');
+      foreach ($contacts as $contact) {
+        $contact_info = explode(',', $contact);
+        echo "<h3>$contact_info[0]</h3><p>$contact_info[1]</p><p>$contact_info[2]</p><p>$contact_info[3]</p>
+      }
+    ?>
 ?>
 </body>
 </html>
