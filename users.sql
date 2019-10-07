22be6318d2b3f59c99207727bce7db075f6862c9
brianhsu0603 <56208363+brianhsu0603@users.noreply.github.com>
2019-10-07 15:07:46 -0700

    Delete users.sql

--- users.sql
+++
@@ -1,4 +0,0 @@
-CREATE DATABASE users;
-USE users;
-CREATE TABLE user ( username varchar(20) not null, password varchar(20) not null );
-INSERT INTO user ( username, password ) VALUES ( 'brianhsu', 'abc' );
