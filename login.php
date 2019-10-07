d96e4ec525687aae7e7c520bf5b76ed1e3aa5e18
brianhsu0603 <56208363+brianhsu0603@users.noreply.github.com>
2019-10-07 01:31:28 -0700

    Update login.php

--- login.php
+++ login.php
@@ -10,7 +10,7 @@
       $myusername = mysqli_real_escape_string($db,$_POST['username']);
       $mypassword = mysqli_real_escape_string($db,$_POST['userpassword']); 
       
-      $sql = "SELECT user FROM users WHERE username = '$myusername' and userpassword = '$mypassword'";
+      $sql = "SELECT username FROM user WHERE username = '$myusername' and userpassword = '$mypassword'";
       $result = mysqli_query($db,$sql);
       $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
       $active = $row['active'];
