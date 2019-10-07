6a6e91517c3669fd439befe57792832ed32c5394
brianhsu0603 <56208363+brianhsu0603@users.noreply.github.com>
2019-10-07 01:09:52 -0700

    Add files via upload

---
+++ users.sql
@@ -0,0 +1,4 @@
+ CREATE DATABASE dbname;
+USE dbname;
+CREATE TABLE users ( username smallint unsigned not null auto_increment, userpassword varchar(20) not null );
+INSERT INTO tablename ( username, userpassword ) VALUES ( "brianhsu", "0000" );
