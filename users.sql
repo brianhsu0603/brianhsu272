4a901009743ec0373aeecb25c484090cb1ad100c
brianhsu0603 <56208363+brianhsu0603@users.noreply.github.com>
2019-10-07 01:52:17 -0700

    Update users.sql

--- users.sql
+++ users.sql
@@ -1,4 +1,4 @@
 CREATE DATABASE users;
 USE users;
 CREATE TABLE user ( username varchar(20) not null, password varchar(20) not null );
-INSERT INTO user ( username, password ) VALUES ( 'brianhsu', '0000' );
+INSERT INTO user ( username, password ) VALUES ( 'brianhsu', 'abc' );
