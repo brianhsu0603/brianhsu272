c1eee538d73e3d1160c5ffbc18bec73ae14ee349
brianhsu0603 <56208363+brianhsu0603@users.noreply.github.com>
2019-10-07 01:17:14 -0700

    Update config.php

--- config.php
+++ config.php
@@ -2,6 +2,6 @@
    define('DB_SERVER', 'localhost:3306');
    define('DB_USERNAME', 'root');
    define('DB_PASSWORD', 'asdzxc0603');
-   define('DB_DATABASE', 'users.sql');
+   define('DB_DATABASE', 'users');
    $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 ?>
