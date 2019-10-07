ce5488d3dec34786c70573651deb1671ce844d1c
brianhsu0603 <56208363+brianhsu0603@users.noreply.github.com>
2019-10-07 15:08:17 -0700

    Delete config.php

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-   define('DB_SERVER', 'localhost:3306');
-   define('DB_USERNAME', 'root');
-   define('DB_PASSWORD', 'asdzxc0603');
-   define('DB_DATABASE', 'users.sql');
-   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-?>
