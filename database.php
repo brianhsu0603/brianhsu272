bac479793eb270230fef3543fe7f77b45a7cf0da
brianhsu0603 <56208363+brianhsu0603@users.noreply.github.com>
2019-10-26 00:04:05 -0700

    Delete database.php

--- database.php
+++
@@ -1,9 +0,0 @@
-<?php
-extract($_POST);
-$query = "insert into users values(".$first_name.",".$last_name.",".$email.",".$home_address.",".$home_phone.",".$cell_phone.");";
-$database_connection = mysqli_connect("localhost","root","asdzxc0603","smarthome");
-$result = mysqli_query($query, "smarthome");
-
-if(!($database_connection = mysqli_connect("localhost","root","asdzxc0603","smarthome")))
-die("couldn't connect to database");
-?>
