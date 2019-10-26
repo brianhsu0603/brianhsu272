abb6ae98be5ee36fa7d6131c202ea708fe0b2703
brianhsu0603 <56208363+brianhsu0603@users.noreply.github.com>
2019-10-26 01:13:47 -0700

    Add files via upload

---
+++ form.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+
+<html>
+  <head>
+    <title>Form vaildation</title>
+</head>
+
+<body>
+  <?php
+   extract($_POST);
+   ?>
+
+   <p>Hi
+     <strong>
+       <?php print("$first_name");?>
+</strong>
+Thank you for registerin.<br />
+you have been added to the
+<strong>
+  <?php print("$users");?>
+</strong>
+<strong>the following information has been saved in our database:</strong><br/>
+<table border = "0" cellpaddong = "0" cellspacing = "10">
+<tr>
+  <td>first_name</td>
+  <td>last_name</td>
+  <td>email</td>
+  <td>home_address</td>
+  <td>home_phone</td>
+  <td>cell_phone</td>
+</tr>
+
+<tr>
+  <?php
+   print("<td>$first_name</td>
+   <td>$last_name</td>
+   <td>$email</td>
+   <td>$home_address</td>
+   <td>$home_phone</td>
+   <td>$cell_phone</td>
+   ");
+   ?>
+   </tr>
+</table>
+</body>
+</html>
