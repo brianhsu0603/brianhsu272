3b18f2e41bc4bff97f28745bd744f8a12aa4ed23
brianhsu0603 <56208363+brianhsu0603@users.noreply.github.com>
2019-10-25 21:08:31 -0700

    Update database.php

--- database.php
+++ database.php
@@ -3,4 +3,7 @@ extract($_POST);
 $query = "insert into users values(".$first_name.",".$last_name.",".$email.",".$home_address.",".$home_phone.",".$cell_phone.");";
 $database_connection = mysqli_connect("localhost","root","asdzxc0603","smarthome");
 $result = mysqli_query($query, "smarthome");
+
+if(!($database_connection = mysqli_connect("localhost","root","asdzxc0603","smarthome")))
+die("couldn't connect to database");
 ?>
