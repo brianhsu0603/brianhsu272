b0b87b5fe78db2f0489103787f892d235885b203
brianhsu0603 <56208363+brianhsu0603@users.noreply.github.com>
2019-12-04 21:46:45 -0800

    Update p9.php

--- p9.php
+++ p9.php
@@ -6,6 +6,9 @@ include 'cookies.php';
 <!DOCTYPE html>
 <html>
     <body>
+           <?php
+        include 'index.php';
+        ?>
 <h1>9.swim Robot:</h1><br>
 <p1>robot that swims.</p1><br>
 <img src="swim.jpg"><br>
