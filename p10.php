19fab85f1d0e1c2da44a4c9d7d5f2db9ea26378e
brianhsu0603 <56208363+brianhsu0603@users.noreply.github.com>
2019-12-04 21:44:20 -0800

    Update p10.php

--- p10.php
+++ p10.php
@@ -6,6 +6,9 @@ include 'cookies.php';
 <!DOCTYPE html>
 <html>
     <body>
+           <?php
+        include 'index.php';
+        ?>
 <h1>10.baseball Robot:</h1><br>
 <p1>robot that plays baseball.</p1><br>
 <img src="baseball.jpg"><br>
