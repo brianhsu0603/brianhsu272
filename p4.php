7dece813dda25ac98178a354fe32f46402ab1e3b
brianhsu0603 <56208363+brianhsu0603@users.noreply.github.com>
2019-12-04 21:45:06 -0800

    Update p4.php

--- p4.php
+++ p4.php
@@ -6,6 +6,9 @@ include 'cookies.php';
 <!DOCTYPE html>
 <html>
     <body>
+           <?php
+        include 'index.php';
+        ?>
 <h1>4.soccer robot:</h1><br>
 <p1>robot that can play soccer.</p1><br>
 <img src="soccer.jpg"><br>
