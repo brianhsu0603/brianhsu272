fa31119412f99fc618f5b44ee9ab6fc8f74cf9cd
brianhsu0603 <56208363+brianhsu0603@users.noreply.github.com>
2019-12-04 21:45:23 -0800

    Update p5.php

--- p5.php
+++ p5.php
@@ -7,6 +7,9 @@ include 'cookies.php';
 <!DOCTYPE html>
 <html>
     <body>
+           <?php
+        include 'index.php';
+        ?>
 <h1>5.Basketball Robot:</h1><br>
 <p1>Robot that play basketball.</p1><br>
 <img src="basketball.jpg"><br>
