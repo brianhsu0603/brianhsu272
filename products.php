e7833140731e4ce4a484b9f53b178a8e97fa4aa0
brianhsu0603 <56208363+brianhsu0603@users.noreply.github.com>
2019-12-04 21:47:48 -0800

    Update products.php

--- products.php
+++ products.php
@@ -1,32 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <style>
-ul {
-  list-style-type: none;
-  margin: 0;
-  padding: 0;
-  overflow: hidden;
-  background-color: #333;
-}
-li {
-  float: left;
-}
-li a {
-  display: block;
-  color: white;
-  text-align: center;
-  padding: 14px 16px;
-  text-decoration: none;
-}
-/* Change the link color to #111 (black) on hover */
-li a:hover {
-  background-color: #111;
-}
-.active {
-  background-color: #4CAF50;
-}
-</style>
+
 
 
 
@@ -35,17 +10,9 @@ li a:hover {
 
 </head>
 <body>
-<ul>
-  <li><a class="active" href="index.php">Home</a></li>
-  <li><a href="password.html">Login</a></li>
-  <li><a href="news.php">News</a></li>
-  <li><a href="contacts.php">Contacts</a></li>
-  <li><a href="products.php">Products</a></li>
-  <li><a href="about.php">About</a></li>
-  <li><a href="form.html">Register</a></li>
-  <li><a href="users.php">Users</a></li>
-  <li><a href="allusers.php">All Users</a></li>
-</ul>
+   <?php
+        include 'index.php';
+        ?>
 
 <h1>Products</h1>
   
