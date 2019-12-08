c4e29a9ba83e8b660c147c6a562f5327fd58a41a
brianhsu0603 <56208363+brianhsu0603@users.noreply.github.com>
2019-12-07 20:21:23 -0800

    Add files via upload

---
+++ purchase.php
@@ -0,0 +1,18 @@
+<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+
+<script type="text/javascript">
+
+  function addToCart() {
+        $.post("http://manisharigala.000webhostapp.com/add_to_cart.php", 
+          {token : "<?php echo ($_COOKIE['userToken']);?>",  
+          prod_id : "<?php echo ($productNum + 10);?>"}, 
+          function(result){
+            $("#rtemp").html(result);
+        });
+    }
+
+    $("#cart_button").on("click", function() {
+      addToCart();
+    });
+</script>
