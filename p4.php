f30c02e88766b8bbe03cfa28e5ef68da81022dab
brianhsu0603 <56208363+brianhsu0603@users.noreply.github.com>
2019-12-08 14:24:05 -0800

    Update p4.php

--- p4.php
+++ p4.php
@@ -35,7 +35,7 @@ include 'cookies.php';
 <p1>robot that can play soccer.</p1><br>
 <img src="soccer.jpg"><br>
 <a href="products.php">Back to Products</a></br>
- <button id="cart_button">Add to cart</button>
+ <button class="button" id="cart_button">Add to cart</button>
     <?php
     include 'purchase.php';
    
