2afd1478c2c85be391d28b4404f51fb1f39fcb46
brianhsu0603 <56208363+brianhsu0603@users.noreply.github.com>
2019-12-07 21:04:25 -0800

    Update p10.php

--- p10.php
+++ p10.php
@@ -4,6 +4,7 @@ include 'cookies.php';
 ?>
 
 <!DOCTYPE html>
+<html>
  <head><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></head>
   <link rel="stylesheet" href="rating.css">
   <style>
