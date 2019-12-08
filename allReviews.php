c3a1aac6e0070ee4458550e34004fee3b8ae5e9a
brianhsu0603 <56208363+brianhsu0603@users.noreply.github.com>
2019-12-07 20:09:34 -0800

    Add files via upload

---
+++ allReviews.php
@@ -0,0 +1,34 @@
+R4EMEMBER TO INCLUDE STAR CSS IN STYLE IN HEAD
+
+<?php
+$pid = $_GET['pid'];
+$pageName= $pid;
+$pageTitle="Reviews for {$pid}";
+include('../header.php');
+?>
+
+<div style="text-align: center;"><form>
+<h2>All reviews for SERVICE NUMBER <?php echo $pid; ?></h2>
+
+<?php include 'fetchRating.php'; ?>
+
+<img src="http://manisharigala.000webhostapp.com/trackItem.php?num=<?php echo $servNo;?>" style="display: none">
+<!--<img src="/images/<?php echo $image; ?>" height=<?php echo $w; ?> width=<?php echo $h; ?>><br> -->
+<input type="button" value="Back" onclick="window.location.href='../services.php'" />
+</form></div>
+
+<?php
+$ch = curl_init();
+curl_setopt($ch,CURLOPT_URL,"http://manisharigala.000webhostapp.com/dbfetchReviews.php");
+curl_setopt($ch,CURLOPT_POST,1);
+curl_setopt($ch,CURLOPT_POSTFIELDS,http_build_query(["prod_id"=>$pid,"limit"=>false]));
+curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+$reviews = curl_exec($ch);
+curl_close($ch);
+
+echo $reviews;
+?>
+
+<?php
+include('../footer.php');
+?>
