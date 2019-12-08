a83e13c1baa1b02dc7c033597c7be8e74bcc9e33
brianhsu0603 <56208363+brianhsu0603@users.noreply.github.com>
2019-12-07 21:07:44 -0800

    Update p4.php

--- p4.php
+++ p4.php
@@ -5,18 +5,32 @@ include 'cookies.php';
 
 <!DOCTYPE html>
 <html>
-    <body>
-        <img src="http://manisharigala.000webhostapp.com/trackItem.php?num=14" style="display: none">
-
-           <?php
+  
+    <head><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></head>
+  <link rel="stylesheet" href="rating.css">
+  <style>
+    .checked{
+      color:orange;
+    }
+  </style>
+  <body>
+  <img src="http://manisharigala.000webhostapp.com/trackItem.php?num=11" style="display: none">
+       
+    <?php
         include 'index.php';
+    include 'fetchRating.php';
+    
         ?>
 <h1>4.soccer robot:</h1><br>
 <p1>robot that can play soccer.</p1><br>
 <img src="soccer.jpg"><br>
-<a href="products.php">Products</a>
-       <?php
-       include 'rating_code.php';
+<a href="products.php">Back to Products</a></br>
+ <button id="cart_button">Add to cart</button>
+    <?php
+    include 'purchase.php';
+   
+    include 'fetchReviews.php';
+       
     ?>
     
     </body>
