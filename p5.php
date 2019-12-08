229037d36d21a19faeea20d34431d41c235386e2
brianhsu0603 <56208363+brianhsu0603@users.noreply.github.com>
2019-12-08 14:24:35 -0800

    Update p5.php

--- p5.php
+++ p5.php
@@ -35,8 +35,7 @@ include 'cookies.php';
 <p1>Robot that play basketball.</p1><br>
 <img src="basketball.jpg"><br>
 <a href="products.php">Back to Products</a></br>
- <button id="cart_button">Add to cart</button>
-    <?php
+ <button class="button" id="cart_button">Add to cart</button>    <?php
     include 'purchase.php';
    
     include 'fetchReviews.php';
