e84393708844adb3cd5e49b43f872630f5926959
brianhsu0603 <56208363+brianhsu0603@users.noreply.github.com>
2019-12-07 21:02:11 -0800

    Update p10.php

--- p10.php
+++ p10.php
@@ -4,13 +4,22 @@ include 'cookies.php';
 ?>
 
 <!DOCTYPE html>
-<html>
-    <body>
-        <img src="http://manisharigala.000webhostapp.com/trackItem.php?num=20" style="display: none">
-
-           <?php
+ <head><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></head>
+  <link rel="stylesheet" href="rating.css">
+  <style>
+    .checked{
+      color:orange;
+    }
+  </style>
+  <body>
+  <img src="http://manisharigala.000webhostapp.com/trackItem.php?num=11" style="display: none">
+       
+    <?php
         include 'index.php';
+    include 'fetchRating.php';
+    
         ?>
+       
 <h1>10.baseball Robot:</h1><br>
 <p1>robot that plays baseball.</p1><br>
 <img src="baseball.jpg"><br>
