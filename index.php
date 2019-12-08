f6af573734663a0d50d76033576138f48c06608b
brianhsu0603 <56208363+brianhsu0603@users.noreply.github.com>
2019-12-08 14:41:51 -0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<h1>Smart Home</h1>
+</head>
+<body>
+<?php
+include 'nav.php';
+?>
+
+<p5 align = "center">Let Us Help You Live A Better Life!</p5>
+
+
+
+
+
+</body>
+</html>
