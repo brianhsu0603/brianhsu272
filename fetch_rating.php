fa8487e6dded87ce144f2d07881a01f8ff3b37ef
brianhsu0603 <56208363+brianhsu0603@users.noreply.github.com>
2019-12-07 20:04:22 -0800

    Update fetch_rating.php

--- fetch_rating.php
+++ fetch_rating.php
@@ -3,7 +3,7 @@
 $ch = curl_init();
 curl_setopt($ch,CURLOPT_URL,"http://manisharigala.000webhostapp.com/dbfetch.php");
 curl_setopt($ch,CURLOPT_POST,1);
-curl_setopt($ch,CURLOPT_POSTFIELDS,http_build_query(["prod_id"=>$servNo]));
+curl_setopt($ch,CURLOPT_POSTFIELDS,http_build_query(["prod_id"=>$productNum]));
 curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
 $rating = curl_exec($ch);
 curl_close($ch);
