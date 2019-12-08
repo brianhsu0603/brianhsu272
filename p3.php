878d8aa37cab719573fb6048e241fc8639b86323
brianhsu0603 <56208363+brianhsu0603@users.noreply.github.com>
2019-12-07 21:06:19 -0800

    Update p3.php

--- p3.php
+++ p3.php
@@ -3,22 +3,35 @@ $productNum = 3;
 include 'cookies.php';
 ?>
 
-
 <!DOCTYPE html>
 <html>
-    <body>
-        <img src="http://manisharigala.000webhostapp.com/trackItem.php?num=13" style="display: none">
-
-           <?php
+ <head><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></head>
+  <link rel="stylesheet" href="rating.css">
+  <style>
+    .checked{
+      color:orange;
+    }
+  </style>
+  <body>
+  <img src="http://manisharigala.000webhostapp.com/trackItem.php?num=11" style="display: none">
+       
+    <?php
         include 'index.php';
-        ?>
+    include 'fetchRating.php';
+    
+        ?> 
 <h1>3.Voice Assistant:</h1><br>
 <p1>Do everything on your instructions.</p1><br>
 <img src="voice assistant.jpg"><br>
-<a href="products.php">Products</a>
-       <?php
-       include 'rating_code.php';
+      <a href="products.php">Back to Products</a></br>
+ <button id="cart_button">Add to cart</button>
+    <?php
+    include 'purchase.php';
+   
+    include 'fetchReviews.php';
+       
     ?>
+
     
     </body>
     </html>
