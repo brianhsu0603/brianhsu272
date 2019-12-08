41e43df80e4b22a2dbdce987d2bfb677982aac42
brianhsu0603 <56208363+brianhsu0603@users.noreply.github.com>
2019-12-07 21:04:06 -0800

    Update p2.php

--- p2.php
+++ p2.php
@@ -5,19 +5,26 @@ include 'cookies.php';
 
 <!DOCTYPE html>
 <html>
-    <body>
-        <img src="http://manisharigala.000webhostapp.com/trackItem.php?num=12" style="display: none">
-
-   <?php
+  <head><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></head>
+  <link rel="stylesheet" href="rating.css">
+  <style>
+    .checked{
+      color:orange;
+    }
+  </style>
+  <body>
+  <img src="http://manisharigala.000webhostapp.com/trackItem.php?num=11" style="display: none">
+       
+    <?php
         include 'index.php';
+    include 'fetchRating.php';
+    
         ?>
 <h1>2.Security Robot:</h1><br>
 <p1>Help you secure your house.</p1><br>
 <img src="security robot.jpg"><br>
 <a href="products.php">Products</a>
-            <?php
-       include 'rating_code.php';
-    ?>
+  
    
     
     </body>
